7d4a311988bff1af6c80c6e5e8ca427bb6a99dec
not-relevant-is-it <joel.edwardson1@gmail.com>
2021-11-08 14:10:52 +0000

    added types and rete as peerdependency

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+import { Plugin as RetePlugin } from 'rete/types/core/plugin';
+
+type InstallOptions = {
+  keyboard?: boolean, // use ctrl-z and ctrl-y for undo/redo, default true
+}
+export interface HistoryPlugin extends RetePlugin {
+  install: (editor: NodeEditor, options: InstallOptions) => void,
+}
+
+export class Action {
+  undo: () => any
+  redo: () => any
+}
+
+
+declare const _default: RetePlugin;
+export default _default;
