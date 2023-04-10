fa16f86acfe78302b34cbf10dba50c047cbe4a30
Ni55aN <huccah@gmail.com>
2023-04-10 14:50:44 +0300

    v2.0.0-beta.5

--- rete.config.ts
+++ rete.config.ts
@@ -3,7 +3,7 @@ import { ReteOptions } from 'rete-cli'
 
 export default <ReteOptions>{
   input: 'src/index.ts',
-  name: 'HistoryPlugin',
+  name: 'ReteHistoryPlugin',
   globals: {
     'rete': 'Rete',
     'rete-area-plugin': 'ReteAreaPlugin'
